b8ef9603111e4c8823233cf71cae56e3b95f8221
Universe <universe@MacBook-Air.local>
2018-07-01 20:10:11 -0400

    add pknorm script

--- overall_pipeline.sh
+++ overall_pipeline.sh
@@ -1,12 +1,11 @@
+
+
+script_dir=/storage/home/gzx103/group/software/signorm/bin/
+
 script_dir=/storage/home/gzx103/group/software/vision_mouse/src/
 working_dir=/storage/home/gzx103/scratch/vision/test_pipeline/
 input_dir=/storage/home/gzx103/scratch/vision/test_pipeline/input_5end_rc/
-input_file_list=info_table_all.rc2nbp.txt
-overall_upper=16
-overall_lower=2
 
-
-cd $working_dir
 
 ###### covert reads count to NB p-value
 while read LINE
@@ -15,7 +14,7 @@ do
 	sig2=$(echo "$LINE" | awk '{print $2}')
 	echo $sig1 $sig2
 	time Rscript $script_dir'negative_binomial_p_2r_bgadj.R' $sig1 $input_dir $sig2 $input_dir $sig1
-done < $input_file_list
+done < info_table_all.rc2nbp.txt
 
 
 
@@ -48,7 +47,6 @@ do
 done
 
 
-
 ###### pknorm normalize reference datasets of all marks
 while read LINE
 do
@@ -64,38 +62,19 @@ do
 	cat $sig1 | awk -F '\t' -v OFS='\t' -v ul=$upperlim '{if ($1>=ul) print ul; else print $1}' > $sig1'.upperlim.txt'
 	cat $sig2 | awk -F '\t' -v OFS='\t' -v ul=$upperlim '{if ($1>=ul) print ul; else print $1}' > $sig2'.upperlim.txt' 
 	### peak norm
-	time python $script_dir'peaknorm_rotate_log_ref_mean.py' -n 500000 -a $sig1'.upperlim.txt' -b $sig2'.upperlim.txt' -u $upperlim -l $lowerlim
+	time python $script_dir'peaknorm_rotate_log_ref_mean.py' -w /storage/home/gzx103/scratch/vision/5end/pknorm_16lim/200_noblack.11_22_2017.bed -p $mark'TablePeaksFiltered.txt' -n 500000 -a 1 -b $sig1'.upperlim.txt' -c 1 -d $sig2'.upperlim.txt' -u $upperlim -l $lowerlim
 done < raw_sig_list.txt
 
 
 
 
-###### pknorm across datasets with the same mark & set limit
-for mk in $(cat mark_list.txt)
-do
-	echo $mk
-	while read LINE
-	do
-		sig1=$(echo "$LINE" | awk '{print $1}')
-		sig2=$(echo "$LINE" | awk '{print $2}')
-		sig2_celltype=$(echo "$LINE" | awk '{print $2}' | awk -F '.' -v OFS='\t' '{print $1}')
-		upperlim=100
-		lowerlim=0
-		echo $sig1 
-		echo $sig2
-		echo $sig2_celltype
-		### set upper limit for pknorm input
-		cat $sig1 | awk -F '\t' -v OFS='\t' -v ul=$upperlim '{if ($1>=ul) print ul; else print $1}' > $sig1'.upperlim.txt'
-		cat $sig2 | awk -F '\t' -v OFS='\t' -v ul=$upperlim '{if ($1>=ul) print ul; else print $1}' > $sig2'.upperlim.txt' 
-		### peak norm
-		time python $script_dir'peaknorm_rotate_log_z_mean.py' -n 500000 -a $sig1'.upperlim.txt' -b $sig2'.upperlim.txt' -u $upperlim -l $lowerlim
-		### set upper limit for pknorm output
-		cat $sig2_celltype'.pknorm.txt' | awk -F '\t' -v OFS='\t' -v ul=overall_upper -v ll=overall_lower '{if ($1<ll) print ll; else if ($>ul) print ul; else print $1}' > $sig2_celltype'.pknorm.'ll'_'ul'.txt'
-		### change file names
-		mv $sig2_celltype'.pknorm.txt' $mk'.'$sig2_celltype'.pknorm.txt'
-		mv $sig2_celltype'.pknorm.'ll'_'ul'.txt' $mk'.'$sig2_celltype'.pknorm.'ll'_'ul'.txt'
-	done < $mk'.pknorm_input_list.txt'
-done
 
 
+
+###### pknorm across datasets with the same mark
+
+
+
+
+###### set limit for signals
 
