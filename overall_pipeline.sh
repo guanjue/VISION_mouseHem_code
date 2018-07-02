287a8704f08a0186465df3b6fbc3430b1aa94248
Universe <universe@MacBook-Air.local>
2018-07-01 20:08:03 -0400

    add pknorm script

--- overall_pipeline.sh
+++ overall_pipeline.sh
@@ -20,9 +20,9 @@ done < info_table_all.rc2nbp.txt
 
 ###### convert nbp to Fisher's method merged p-value
 ### extrac cell type mark list
-ls *.nbp_bgadj.txt.nbp_2r_bgadj.txt | awk -F '.' -v OFS='\t' '{print $1"."$2}' | sort -u > cell_marker_list.txt
-ls *.nbp_bgadj.txt.nbp_2r_bgadj.txt | awk -F '.' -v OFS='\t' '{print $2}' | sort -u > marker_list.txt
-ls *.nbp_bgadj.txt.nbp_2r_bgadj.txt | awk -F '.' -v OFS='\t' '{print $1}' | sort -u > cell_list.txt
+ls *.nbp_2r_bgadj.txt | awk -F '.' -v OFS='\t' '{print $1"."$2}' | sort -u > cell_marker_list.txt
+ls *.nbp_2r_bgadj.txt | awk -F '.' -v OFS='\t' '{print $2}' | sort -u > marker_list.txt
+ls *.nbp_2r_bgadj.txt | awk -F '.' -v OFS='\t' '{print $1}' | sort -u > cell_list.txt
 
 ### move data NB p-value data into nbp folder
 if [ -d $working_dir'nbp/' ]; then echo $working_dir'nbp/' exist; else mkdir $working_dir'nbp/'; fi
