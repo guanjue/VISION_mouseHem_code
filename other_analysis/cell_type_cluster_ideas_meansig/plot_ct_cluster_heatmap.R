08ed1271e0fd296df75f1d2c6ad2c04d3b177e1e
guanjue <guanjuexiang@gmail.com>
2018-07-03 16:37:07 -0400

    ideas cell type clustering

---
+++ other_analysis/cell_type_cluster_ideas_meansig/plot_ct_cluster_heatmap.R
@@ -0,0 +1,13 @@
+euclidean_dist = read.table('IDEAS_state_meansignal_Euclidean_dist_matrix.txt', header=FALSE)
+
+colnames(euclidean_dist) = c('B_SPL', 'CFU_E_ad', 'CFUMK', 'CLP', 'CMP', 'ER4', 'ERY_ad', 'ERY_fl', 'G1E', 'GMP', 'HPC7', 'LSK_BM', 'MEP', 'MK_imm_ad', 'MK_mat_fl', 'MONO_BM', 'NEU', 'NK_SPL', 'T_CD4_SPL', 'T_CD8_SPL')
+rownames(euclidean_dist) = c('B_SPL', 'CFU_E_ad', 'CFUMK', 'CLP', 'CMP', 'ER4', 'ERY_ad', 'ERY_fl', 'G1E', 'GMP', 'HPC7', 'LSK_BM', 'MEP', 'MK_imm_ad', 'MK_mat_fl', 'MONO_BM', 'NEU', 'NK_SPL', 'T_CD4_SPL', 'T_CD8_SPL')
+
+library(pheatmap)
+my_colorbar=colorRampPalette(c('white', 'red'))(n = 128)
+col_breaks = c(seq(0, 2000,length=33))
+
+
+pdf('IDEAS_state_meansignal_Euclidean_dist_matrix.pdf')
+pheatmap(euclidean_dist, color=my_colorbar, cluster_cols = TRUE,cluster_rows=TRUE,annotation_names_row=TRUE,annotation_names_col=TRUE,show_rownames=TRUE,show_colnames=TRUE,clustering_method = 'complete')
+dev.off()
