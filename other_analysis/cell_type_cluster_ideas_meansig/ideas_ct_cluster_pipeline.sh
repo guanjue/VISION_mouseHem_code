bae2a25c0ec4f57f76a83f24026f85c9e281ded9
guanjue <guanjuexiang@gmail.com>
2018-07-03 16:12:09 -0400

    ideas cell type clustering

--- other_analysis/cell_type_cluster_ideas_meansig/ideas_ct_cluster_pipeline.sh
+++ other_analysis/cell_type_cluster_ideas_meansig/ideas_ct_cluster_pipeline.sh
@@ -1,11 +1,11 @@
-script_dir=
+script_dir='/storage/home/gzx103/group/software/vision_mouse/other_analysis/cell_type_cluster_ideas_meansig/'
 
 ######
 ### get ideas mean signal heatmap & mean signal matrix
-time Rscript plot_ideas_heatmap.R
+time Rscript $script_dir'plot_ideas_heatmap.R'
 
 
 ######
 ### get mv distance of each bin based on IDEAS state mean signal
-time python ct_dist_ideas_state_mean_signal.py -i pknorm_2_16lim_ref1mo_0424_lesshet.state -s ideas_state_mean_signal.txt -o IDEAS_state_meansignal_Euclidean_dist_matrix.txt
+time python $script_dir'ct_dist_ideas_state_mean_signal.py' -i pknorm_2_16lim_ref1mo_0424_lesshet.state -s ideas_state_mean_signal.txt -o IDEAS_state_meansignal_Euclidean_dist_matrix.txt
 
