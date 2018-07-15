dfb11752555e4923ac0f3089324107355b0d84c2
Universe <universe@MacBook-Air.local>
2018-07-15 16:16:11 -0400

    fdr 1st pk

--- src/negative_binomial_p_2r_bgadj_bayes.R
+++ src/negative_binomial_p_2r_bgadj_bayes.R
@@ -147,7 +147,7 @@ print(input_0_var)
 
 ### get negative binomial p-value 1st round
 #nb_pval = apply(sig_input, MARGIN=1, function(x) pnbinom(x[1], sig_0_size * (x[2]+1)/(input_0_mean+1), sig_0_prob, lower.tail=FALSE) )
-nb_pval = apply(sig_input, MARGIN=1, function(x) pnbinom(x[1], sig_0_size, sig_0_prob, lower.tail=FALSE) )
+nb_pval = apply(sig, MARGIN=1, function(x) pnbinom(x[1], sig_0_size, sig_0_prob, lower.tail=FALSE) )
 
 nb_pval[nb_pval<=1e-324] = 1e-324
 neglog10_nb_pval = -log10(nb_pval)
