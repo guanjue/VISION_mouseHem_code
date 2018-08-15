5373bff8074ea4af7e5db0ca0e7738159ae74ee2
Universe <universe@MacBook-Air.local>
2018-08-15 01:01:14 -0400

    ct bar

--- other_analysis/whole_genome_correlation_heatmap/plot_correlation_heatmap_withcolorbar.R
+++ other_analysis/whole_genome_correlation_heatmap/plot_correlation_heatmap_withcolorbar.R
@@ -46,36 +46,132 @@ color_heatmap = function(color_matrix, outputname, format, border_color){
 
 ####################################################
 ### get color bar based on mark
-get_color_bar = function(mk){
+get_color_bar_mark = function(mk){
 	if (mk == 'h3k4me3'){
-		color_bar = rgb(255/255,0/255,0/255)
+		color_bar_mark = rgb(255/255,0/255,0/255)
 	} else if (mk == 'h3k4me1') {
-		color_bar = rgb(250/255,250/255,0/255)
+		color_bar_mark = rgb(250/255,250/255,0/255)
 	} else if (mk == 'h3k27ac') {
-		color_bar = rgb(250/255,150/255,0/255)
+		color_bar_mark = rgb(250/255,150/255,0/255)
 	} else if (mk == 'h3k27me3') {
-		color_bar = rgb(0/255,0/255,225/255)
+		color_bar_mark = rgb(0/255,0/255,225/255)
 	} else if (mk == 'h3k36me3') {
-		color_bar = rgb(0/255,150/255,0/255)
+		color_bar_mark = rgb(0/255,150/255,0/255)
 	} else if (mk == 'h3k9me3') {
-		color_bar = rgb(100/255,100/255,100/255)
+		color_bar_mark = rgb(100/255,100/255,100/255)
 	} else if (mk == 'atac') {
-		color_bar = rgb(200/255,50/255,150/255)
+		color_bar_mark = rgb(200/255,50/255,150/255)
 	} else if (mk == 'dnase') {
-		color_bar = rgb(0/255,200/255,200/255)
+		color_bar_mark = rgb(0/255,200/255,200/255)
 	} else if (mk == 'ctcf') {
-		color_bar = rgb(200/255,0/255,250/255)
+		color_bar_mark = rgb(200/255,0/255,250/255)
 	} else {
-		color_bar = rgb(0/255,0/255,0/255)
+		color_bar_mark = rgb(0/255,0/255,0/255)
 	}
-	return(color_bar)
+	return(color_bar_mark)
+}
+
+####################################################
+### get color bar based on cell type
+get_color_bar_ct = function(ct){
+	if (ct == 'LSK'){
+		color_bar_ct = rgb(34/255,139/255,34/255)
+	} else if (ct == 'HPC7') {
+		color_bar_ct = rgb(255/255,127/255,0/255)
+	} else if (ct == 'CMP') {
+		color_bar_ct = rgb(238/255,118/255,0/255)
+	} else if (ct == 'MEP') {
+		color_bar_ct = rgb(205/255,102/255,0/255)
+	} else if (ct == 'G1E') {
+		color_bar_ct = rgb(238/255,99/255,99/255)
+	} else if (ct == 'ER4') {
+		color_bar_ct = rgb(205/255,85/255,85/255)
+	} else if (ct == 'CFUE') {
+		color_bar_ct = rgb(255/255,48/255,48/255)
+	} else if (ct == 'ERY_fl') {
+		color_bar_ct = rgb(238/255,44/255,44/255)
+	} else if (ct == 'ERY') {
+		color_bar_ct = rgb(255/255,0/255,0/255)
+	} else if (ct == 'CFUMK') {
+		color_bar_ct = rgb(139/255,115/255,85/255)
+	} else if (ct == 'iMK') {
+		color_bar_ct = rgb(139/255,90/255,43/255)
+	} else if (ct == 'MK_fl') {
+		color_bar_ct = rgb(139/255,69/255,19/255)
+	} else if (ct == 'GMP') {
+		color_bar_ct = rgb(0/255,139/255,139/255)
+	} else if (ct == 'CLP') {
+		color_bar_ct = rgb(147/255,112/255,219/255)
+	} else if (ct == 'MON') {
+		color_bar_ct = rgb(24/255,116/255,205/255)
+	} else if (ct == 'NEU') {
+		color_bar_ct = rgb(79/255,148/255,205/255)
+	} else if (ct == 'B') {
+		color_bar_ct = rgb(139/255,28/255,98/255)
+	} else if (ct == 'NK') {
+		color_bar_ct = rgb(139/255,0/255,139/255)
+	} else if (ct == 'T_CD4') {
+		color_bar_ct = rgb(122/255,55/255,139/255)
+	} else if (ct == 'T_CD8') {
+		color_bar_ct = rgb(104/255,34/255,139/255)
+	} 
+	return(color_bar_ct)
+}
+
+####################################################
+### get color bar based on mark
+change_ct_names = function(ct){
+	if (ct == 'B_SPL') {
+		color_bar_mark = 'B'
+	} else if (ct == 'CFU_E_ad') {
+		color_bar_mark = 'CFUE'
+	} else if (ct == 'CFUMK') {
+		color_bar_mark = 'CFUMK'
+	} else if (ct == 'CLP') {
+		color_bar_mark = 'CLP'
+	} else if (ct == 'CMP') {
+		color_bar_mark = 'CMP'
+	} else if (ct == 'ER4') {
+		color_bar_mark = 'ER4'
+	} else if (ct == 'ERY_ad') {
+		color_bar_mark = 'ERY'
+	} else if (ct == 'ERY_fl') {
+		color_bar_mark = 'ERY_fl'
+	} else if (ct == 'G1E') {
+		color_bar_mark = 'G1E'
+	} else if (ct == 'GMP') {
+		color_bar_mark = 'GMP'
+	} else if (ct == 'HPC7') {
+		color_bar_mark = 'HPC7'
+	} else if (ct == 'LSK_BM') {
+		color_bar_mark = 'LSK'
+	} else if (ct == 'MEP') {
+		color_bar_mark = 'MEP'
+	} else if (ct == 'MK_imm_ad') {
+		color_bar_mark = 'iMK'
+	} else if (ct == 'MK_mat_fl') {
+		color_bar_mark = 'MK_fl'
+	} else if (ct == 'MONO_BM') {
+		color_bar_mark = 'MON'
+	} else if (ct == 'NEU') {
+		color_bar_mark = 'NEU'
+	} else if (ct == 'NK_SPL') {
+		color_bar_mark = 'NK'
+	} else if (ct == 'T_CD4_SPL') {
+		color_bar_mark = 'T_CD4'
+	} else if (ct == 'T_CD8_SPL') {
+		color_bar_mark = 'T_CD8'
+	} 
+	return(color_bar_mark)
 }
 
 ###### read file list file
 data = read.table(signal_file_list, header = F)
 data_matrix = NULL
 data_name = c()
-color_bar = c()
+color_bar_mark = c()
+color_bar_ct = c()
+ct_name_vec = c()
 for (i in c(1: dim(data)[1])){
         file = paste( toString(data[i,1]), sep='')
         print(file)
@@ -85,9 +181,14 @@ for (i in c(1: dim(data)[1])){
         data_matrix = cbind(data_matrix, d_tmp)
         ###### split file name
         filename_split_vec = unlist(strsplit(file, "[.]"))
-        data_name[i] = paste(filename_split_vec[1], filename_split_vec[2], sep='_')
+        ct_tmp = filename_split_vec[1]
+        ct_tmp_modified = change_ct_names(ct_tmp)
+        mk_tmp = filename_split_vec[2]
+        data_name[i] = paste(ct_tmp_modified, mk_tmp, sep='_')
         ##### get color bar based on mark
-        color_bar = rbind(color_bar, get_color_bar(filename_split_vec[2]))
+        color_bar_mark = rbind(color_bar_mark, get_color_bar_mark(mk_tmp))
+        color_bar_ct = rbind(color_bar_ct, get_color_bar_ct(ct_tmp_modified))
+        ct_name_vec = rbind(ct_name_vec, ct_tmp_modified)
 }
 
 
@@ -156,14 +257,16 @@ for (i in seq(1,dim(r)[2])){
 
 ###### reorder correlation heatmap by the correlation-dist  
 signal_matrix_color_reorder = signal_matrix_color[new_ct_order, new_ct_order]
-color_bar_reorder = color_bar[new_ct_order]
+color_bar_mark_reorder = color_bar_mark[new_ct_order]
+color_bar_ct_reorder = color_bar_ct[new_ct_order]
 
 ###### merge corlor bar and correlation matrix
-signal_matrix_color_reorder = cbind(signal_matrix_color_reorder, color_bar_reorder)
+signal_matrix_color_reorder = cbind(signal_matrix_color_reorder, color_bar_mark_reorder, color_bar_ct_reorder)
 
 ###### add colnames row names
-colnames(signal_matrix_color_reorder) = c(data_name[new_ct_order], 'mark')
-rownames(signal_matrix_color_reorder) = data_name[new_ct_order]
+colnames(signal_matrix_color_reorder) = c(data_name[new_ct_order], 'mark', 'cell-type')
+ct_name_vec_reorder = ct_name_vec[new_ct_order]
+rownames(signal_matrix_color_reorder) = ct_name_vec_reorder
 
 
 ####################################################
