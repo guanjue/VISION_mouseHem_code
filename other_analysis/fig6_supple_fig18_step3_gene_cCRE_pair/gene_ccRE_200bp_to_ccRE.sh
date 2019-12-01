323588b723d6b51506f27e7591238b55c305cbf1
Universe <universe@Guanjues-MacBook-Pro.local>
2019-12-01 02:18:16 -0500

    eRP fig6 supplefig18 scripts

--- other_analysis/fig6_supple_fig18_step3_gene_cCRE_pair/gene_ccRE_200bp_to_ccRE.sh
+++ other_analysis/fig6_supple_fig18_step3_gene_cCRE_pair/gene_ccRE_200bp_to_ccRE.sh
@@ -23,6 +23,14 @@ time sort -u all_converted.txt | sort -k3,3 > all_converted.sort.txt
 
 ### get group
 time python 200bp_to_ccRE.geneGroup.py
+
+### get cCRE state
+cat all_converted.sort.g.txt | awk -F '\t' -v OFS='\t' '{print $1"_"$4"_"$5}' > allcCRE_list.txt
+cat /storage/home/gzx103/group/projects/vision/snapshot20_reproduce_16lim_pre_0state_ccRE_is_overestimated/atac_20cell.fun.txt | awk -F '\t' -v OFS='\t' '{print $1"_"$2"_"$3, $5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24}' > atac_fun.matrix.txt
+time python vlookup.py -t atac_fun.matrix.txt -m 1 -s allcCRE_list.txt -n 1 -o all_converted.sort.g.cCRE_state.txt -k F
+paste all_converted.sort.g.txt all_converted.sort.g.cCRE_state.txt > all_converted.sort.g.cCRE_state.txt.tmp && mv all_converted.sort.g.cCRE_state.txt.tmp all_converted.sort.g.cCRE_state.txt
+
+### split by gene group
 cat all_converted.sort.g.txt | awk -F '\t' -v OFS='\t' '{if ($8==1) print $0}' > all_converted.sort.g1.txt
 cat all_converted.sort.g.txt | awk -F '\t' -v OFS='\t' '{if ($8==2) print $0}' > all_converted.sort.g2.txt
 cat all_converted.sort.g.txt | awk -F '\t' -v OFS='\t' '{if ($8==3) print $0}' > all_converted.sort.g3.txt
--- other_analysis/fig6_supple_fig18_step4_eRP_track_scripts/get_eRP_for_each_cCRE_in_each_ct.sh
+++ other_analysis/fig6_supple_fig18_step4_eRP_track_scripts/get_eRP_for_each_cCRE_in_each_ct.sh
@@ -2,6 +2,8 @@
 folder_of_Rdat='/gpfs/group/yzz2/default/legacy/group/projects/vision/rna/'
 time Rscript regress_coefficient_2kb.with0.R $folder_of_Rdat
 
+### get cCRE state proportion
+cd /storage/home/gzx103/group/projects/vision/pknorm_2_16lim_ref1mo_sample1mo_0424/get_eRP_tracks
 ### get state counts in cCRE in each ct
 qsub PBS.splitbed.sh
 
