bd37e5669bd445dca9e1dd1526ad72741c9230fe
Universe <universe@Guanjues-MacBook-Pro.local>
2020-02-10 15:11:18 -0500

    clean

--- fig6_supple_fig18_step1_ccRE_selection_script/run3.R
+++
@@ -1,3 +0,0 @@
-source("degene_try.R");
-args<-commandArgs(trailingOnly=TRUE);
-DEpredict(args[1], "/gpfs/scratch/yzz2/encode_mouse/","me66n_org.",frna="me66_gene_tpm_full.txt",fpref="me66norg",maxdist=100000,gidlist=as.integer(args[2]):as.integer(args[3]));
--- fig6_supple_fig18_step7_get_megatable/round.R
+++
@@ -1,6 +0,0 @@
-d = read.table('ccRE_eRP.table.txt', header=F)
-d_sig = d[,-1]
-d_sig_r = round(d_sig, 2)
-d = cbind(d[,1], d_sig_r)
-write.table(d, 'ccRE_eRP.table.r.txt', quote=F, col.names=F, row.names=F, sep='\t')
-
