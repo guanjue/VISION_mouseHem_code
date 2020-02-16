82d8ac3d336499b38f2e87f0afe5c30a716dbfe4
Universe <universe@Guanjues-MacBook-Pro.local>
2020-02-16 15:51:23 -0500

    add IDEAS and S3norm pipeline

---
+++ 02_IDEAS_pipeline/RUNME.sh
@@ -0,0 +1,8 @@
+### run IDEAS
+time bash run_IDEAS.sh
+
+### rerun IDEAS after moving heterogeneous states from the para file, 
+### Then use the para file as the prior (line 20) in the "run_IDEAS_lesshetero.parafile" file
+### Then rerun IDEAS
+time bash run_IDEAS_lesshetero.sh
+
